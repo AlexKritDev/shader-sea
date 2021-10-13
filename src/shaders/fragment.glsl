6e89a9856277878a94afcef6968b03505c33fea6
AlexKritDev <akrit.developer@gmail.com>
2021-10-13 19:33:01 +0300

    done

---
+++ src/shaders/fragment.glsl
@@ -0,0 +1,23 @@
+//uniforms
+uniform vec3 uSurfaceColor;
+uniform vec3 uDepthColor;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
+
+
+
+//varying
+varying float vElevation;
+
+
+//functions
+
+
+void main() {
+
+float mixStrength = (vElevation   + uColorOffset)  * uColorMultiplier;
+vec3 color = mix(uDepthColor, uSurfaceColor,mixStrength);
+
+gl_FragColor = vec4(color,1.0);
+
+}
